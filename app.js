0af141c7d4f0b5f6814b1013a02f27ceb6349be5
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-05-23 16:00:38 +0500

    8

--- app.js
+++ app.js
@@ -11,11 +11,52 @@
 
 // //******Q3 ***********************//
 
-var text = "Pakistan"
-alert(text)
-alert("The Index Of n is :" + text.indexOf("n"))
+// var text = "Pakistan"
+// alert(text)
+// alert("The Index Of n is :" + text.indexOf("n"))
 
 // //**************Q4************ *//
 
-// var text = "Hello World"
-// alert(text + '\n' + 'The last Index Of l is : ' + '\n' + text.lastIndexOf("l"))
+// var text1 = "Hello World"
+// alert(text1 + '\n' + 'The last Index Of l is : ' + '\n' + text1.lastIndexOf("l"))
+
+// //**************Q5************ *//
+
+// var wordPakistan = "pakistan"
+// alert('The Character at address of i in word pakistan is :' + '\n' + wordPakistan.charAt('3'))  
+
+// //**************Q6************ *//
+
+// var firtName1 = prompt("Enter Your First Name")
+// var lastName1 = prompt("Enter Yor Last Name")
+// alert("Hello " + firtName1.concat(lastName1) + " Wellcome")
+// COCAT METHOD MA DO STRING KA BEECH MA GAAP KAISA AI GA?
+
+// //**************Q7************ *//
+
+// var writtenText = "the city is hyderabad";
+// var wordSelect = prompt(writtenText + '\n' + "Which Word Do You Want To replace");
+// var newWord = prompt("Which Word Do You Want To Add");
+// var newWordLowercased = wordSelect.toLowerCase();
+// var index = writtenText.indexOf(newWordLowercased);
+
+// // console.log(index);
+
+// if(index !== -1){
+//     writtenText = writtenText.slice(0, index) + newWord + writtenText.slice(index + wordSelect.length);
+//     alert(writtenText);
+// }
+
+// else{
+//     alert("not found")
+// }
+
+
+// //**************Q8************ *//
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var messageReplaced = message.replace('and','&');
+// for(var i = 0 ; i > message.length; i++){
+//     if(message[i] === 'and'){
+        
+//     }()
+
