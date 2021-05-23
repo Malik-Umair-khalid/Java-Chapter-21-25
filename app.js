2282824cfcba30d0193fbf22d4d407ca98a7cef3
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-05-23 12:59:26 +0500

    Question 1 Completed

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-// 
+// // *********Q1******************//
+
+// var firtName = prompt("Enter Your First Name")
+// var lastName = prompt("Enter Yor Last Name")
+// alert("Hello" + firtName.toUpperCase() + lastName.toUpperCase() + "Wellcome")
+
 // //**************Q2 ************//
+
 // var phoneName = prompt("Enter Your Favourite Phone Name")
 // alert("The Length Of The String Is :" + '\n' + phoneName.length)
+
+//******Q3 ***********************//
+
